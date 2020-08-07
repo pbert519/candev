6efaa40fcdab05a0f6cd220b8e7a7b5998466ed1
René Herrero Gómez <rene@sevenseas.io>
2020-08-07 13:04:45 -0500

    fixed the driver

--- examples/driver.rs
+++ examples/driver.rs
@@ -1,7 +1,9 @@
 use candev::Socket;
 use embedded_hal::can::{Frame, Receiver, Transmitter};
+use nb::block;
 use std::{thread, time};
 
+#[derive(Debug)]
 struct Driver<T> {
     pub id: u32,
     pub t: T,
@@ -10,26 +12,15 @@ struct Driver<T> {
 impl<T> Driver<T>
 where
     T: Receiver + Transmitter,
+    <T as Transmitter>::Error: core::fmt::Debug,
 {
     pub fn echo(&mut self) {
         let mut count = 0;
         while count < 3 {
-            /* This doesn't work:
-            if let Ok(frame) = self.t.receive() {
-                self.t.transmit(&frame);
-            }
-            */
             if let Ok(frame) = self.t.receive() {
                 let frame = <T as Transmitter>::Frame::new_standard(self.id, frame.data()).unwrap();
                 thread::sleep(time::Duration::from_secs(1));
-                //FIXME: Adding .unwrap() gives an error???
-                match self.t.transmit(&frame) {
-                    Ok(f) => match f {
-                        None => {}
-                        Some(_) => panic!("Transmit returned a queued frame"),
-                    },
-                    Err(_) => panic!("Failed to transmit frame"),
-                }
+                block!(self.t.transmit(&frame)).unwrap();
             }
 
             count += 1;
@@ -56,5 +47,4 @@ fn main() {
 
     child1.join().unwrap();
     child2.join().unwrap();
-    
 }
